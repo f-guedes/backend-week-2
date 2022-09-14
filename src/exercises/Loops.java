991f990cf34fab94ee7f5f15879803fc099a82e1
f-guedes <fernando.hpg2@gmail.com>
2022-09-14 00:25:23 -0400

    exercises: loops

---
+++ src/exercises/Loops.java
@@ -0,0 +1,39 @@
+package exercises;
+
+public class Loops {
+
+	public static void main(String[] args) {
+		// for loops
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+		}
+		
+		for (int i = 1; i <=10; i++) {
+			System.out.println(i);
+		}
+		
+		for (int i = 0; i <= 20; i++) {
+			if (i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+				// end //
+		
+		
+		// while loop
+		int x = 0;
+		while (x <= 10) {
+			System.out.println(x);
+			x++;	
+		}
+		
+		// do while loop
+		do {
+			System.out.println(x);
+			x++;
+		} while (x <= 20);
+		
+		
+	}
+
+}
